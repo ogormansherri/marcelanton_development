9b607dc1e650730b528c4e840771c45845649263
Sherri Ann Gunder <verdu103@mail.chapman.edu>
2015-10-17 11:25:19 -0700

    Update form.php

--- php/form.php
+++ php/form.php
@@ -1,1 +1,20 @@
-
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $from = 'http://verdu103.github.io/marcelanton_development';
+  $to = "sverduo@me.com"
+  $subject = 'Website Design Information Email Inquiry'
+  
+  $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+  ?>
+  
+  <?php
+    if ($_POST['submit']){
+      if (mail ($to, $subject, $body, $from)){
+        echo '<p>Thank you for your email!</p>';
+      } else {
+        echo '<p>Oops! An error occurred. Try sending your message again.</p>'; 
+      }
+    }
+    ?>
